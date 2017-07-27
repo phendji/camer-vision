977ea646eb72aeaae37fbd70553de8455f1c89f5
Patrick Hendji <patrick.hendji@capgemini.com>
2017-07-27 19:26:30 +0200

    Ajout de la page d'admin

--- public/modules/admin/adminCtrl.js
+++ public/modules/admin/adminCtrl.js
@@ -2,5 +2,13 @@
 var app = angular.module('admin', []);
 
 app.controller('adminCtrl', ['$scope', '$rootScope', 'mainServices', function($scope, $rootScope, mainServices) {
-    console.log("admin page");
+  console.log("admin page");
+
+  //Page-Level Demo Scripts - Tables - Use for reference
+	$(document).ready(function() {
+    $('#dataTables-proposition').DataTable({
+       responsive: true
+    });
+	});
+
 }]);
